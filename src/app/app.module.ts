d6e242bd6e790f0cec7d34ca223bb43911926f72
Courtney Burns <courtneylynnburns@gmail.com>
2018-03-08 08:42:11 -0600

    autocreated success component with cli shortcut

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { WarningComponent } from './warning/warning.component';
+import { SuccessComponent } from './success/success.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    WarningComponent
+    WarningComponent,
+    SuccessComponent
   ],
   imports: [
     BrowserModule,
